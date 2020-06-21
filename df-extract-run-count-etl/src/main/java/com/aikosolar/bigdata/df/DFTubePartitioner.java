9e8cea87e0c6bf2e2f32f848c254497f1d37f527
stevan <734506700qq@gmail.com>
2020-06-21 22:16:44 +0800

    修复发送partation错误

--- df-extract-run-count-etl/src/main/java/com/aikosolar/bigdata/df/DFTubePartitioner.java
+++ df-extract-run-count-etl/src/main/java/com/aikosolar/bigdata/df/DFTubePartitioner.java
@@ -9,6 +9,6 @@ import org.apache.flink.streaming.connectors.kafka.partitioner.FlinkKafkaPartiti
 public class DFTubePartitioner extends FlinkKafkaPartitioner<DFTube> {
     @Override
     public int partition(DFTube tube, byte[] bytes, byte[] bytes1, String s, int[] ints) {
-        return Integer.parseInt(tube.id);
+        return tube.eqpID.hashCode();
     }
 }
